89307abad0de665e782820ca8e8e500815d9ba2a
devdiagon <fredericktimo@gmail.com>
2026-01-16 10:42:13 -0500

    fix: updated api endpoints

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,12 +1,12 @@
 class ApiConfig {
-  static const String baseUrl = 'https:/127.0.0.1:3000';
+  static const String baseUrl = 'http://127.0.0.1:3000';
   static const String apiPath = '/poliza/api';
 
   static String get fullApiUrl => '$baseUrl$apiPath';
 
   // Endpoints
-  static const String automovil = '/automovil';
-  static const String seguro = '/seguro';
+  static const String automovil = '/vehicle';
+  static const String seguro = '/insurance';
 
   static String seguroById(String id) => '$seguro/$id';
 }
